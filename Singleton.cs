5d24113b40887d66f28478345a5b4fe8f606a58a
bsoli <benjamintsoli@gmail.com>
2021-02-18 13:40:32 -0500

    Add files via upload

---
+++ Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+    public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+
+        public static T Instance { get; private set; }
+
+
+        public static bool IsInitialized
+        {
+            get { return Instance != null; }
+        }
+        // Start is called before the first frame update 
+        void Start()
+        {
+            Instance = (T)this;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (Instance == this)
+            {
+                Instance = null;
+            }
+        }
+    }
